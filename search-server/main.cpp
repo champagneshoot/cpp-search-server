a646ff5374205e7576876747071dce17beebd002
champagneshoot <162992559+champagneshoot@users.noreply.github.com>
2024-03-23 18:50:41 +0300

    Update main.cpp
    изменил название переменной в функции AddDocument с tf на variable_for_counting_tf и изменил название функции IDF на CountIDF

--- search-server/main.cpp
+++ search-server/main.cpp
@@ -80,11 +80,11 @@ public:
     {
         const vector<string> words = SplitIntoWordsNoStop(document);
         const double vector_size = words.size();
-        double tf = 1 / vector_size; // исправил имя функции
+        double variable_for_counting_tf = 1 / vector_size;
 
         for (const string& word : words)
         {
-            documents_[word][document_id] += tf; // убрал подсчет 1/size внутри цикла
+            documents_[word][document_id] += variable_for_counting_tf;
         }
         document_count_++;
     }
@@ -146,7 +146,7 @@ private:
         return query_words;
     }
 
-    double IDF(const string word) const // вевел расчет IDF в отдельную функцию
+    double CountIDF(const string word) const
     {
         return log(static_cast<double>(document_count_) / documents_.at(word).size());
     }
@@ -159,7 +159,7 @@ private:
         {
             if (documents_.count(word) > 0)
             {
-                double idf = IDF(word);
+                double idf = CountIDF(word);
                 for (auto& [id, tf] : documents_.at(word))
                 {
                     document_to_relevance[id] += tf * idf;
